02029cba0a237ab6ffaa5cce137f7ce44bec6570
JustiqGrizzy <justiqgrizzy@gmail.com>
2024-06-11 17:29:53 +0500

    Optimization

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,14 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Loader } from "../ui";
-import { useNavigate } from "react-router-dom";
 import ArticleService from "../service/article";
 import { getArticlesStart, getArticlesSucces } from "../slice/article";
+import { ArticleCard } from "./";
 
 const Main = () => {
   const { articles, isLoading } = useSelector((state) => state.article);
-  const { loggedIn, user } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
 
-  const deleteArticle = async (slug) => {
-    try {
-      await ArticleService.deleteArticle(slug);
-      getArticles();
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const dispatch = useDispatch();
 
   const getArticles = async () => {
     dispatch(getArticlesStart());
@@ -39,58 +29,7 @@ const Main = () => {
       {isLoading && <Loader />}
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
         {articles.map((item) => (
-          <div className="col " key={item.id}>
-            <div className="card h-100 shadow-sm">
-              <svg
-                className="bd-placeholder-img card-img-top"
-                width="100%"
-                height="225"
-                xmlns="http://www.w3.org/2000/svg"
-                role="img"
-                aria-label="Placeholder: Thumbnail"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-              >
-                <rect width="100%" height="100%" fill="#55595c"></rect>
-              </svg>
-              <div className="card-body d-block  ">
-                <div className="card-title fw-bold">{item.title}</div>
-                <p className="card-text ">{item.description}</p>
-              </div>
-              <div className="card-footer d-flex justify-content-between align-items-center">
-                <div className="btn-group">
-                  <button
-                    type="button"
-                    className="btn btn-sm btn-outline-success"
-                    onClick={() => navigate(`/article/${item.slug}`)}
-                  >
-                    View
-                  </button>
-                  {loggedIn && item.author.username === user.username && (
-                    <>
-                      <button
-                        type="button"
-                        className="btn btn-sm btn-outline-secondary"
-                        onClick={() => navigate(`/edit-article/${item.slug}`)}
-                      >
-                        Edit
-                      </button>
-                      <button
-                        type="button"
-                        className="btn btn-sm btn-outline-danger"
-                        onClick={() => deleteArticle(item.slug)}
-                      >
-                        Delete
-                      </button>
-                    </>
-                  )}
-                </div>
-                <small className="text-body-secondary fw-bold text-capitalize">
-                  {item.author.username}
-                </small>
-              </div>
-            </div>
-          </div>
+          <ArticleCard item={item} getArticles={getArticles} />
         ))}
       </div>
     </div>
